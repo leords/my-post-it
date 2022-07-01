6e776d43d5cef63487f9ef66b88b313d17761a1c
leords <leo_ro.drigues@hotmail.com>
2022-06-30 22:02:55 -0300

    Modify header component

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,6 +1,5 @@
 import { SignOut } from "phosphor-react"
-import React, { useContext, useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useContext } from "react"
 import { AuthContext } from "../Auth/AuthContext"
 
 interface User {
@@ -17,12 +16,13 @@ interface User {
 export function Header() {
 
     const {signOut, user} = useContext(AuthContext)
+    const nameUser: User = Object.create(user)
 
     return (
         <div className=" flex flex-row bg-white w-full p-10 shadow-md">
             <h1 className=" flex text-6xl font-black text-black">post-it </h1>
             <div className="flex flex-col items-center justify-center w-20 h-16 absolute right-20">
-                <p className="text-gray-600 text-xs py-4">{user?.user.email}</p>
+                <p className="text-gray-600 text-xs py-4">{nameUser.user.name}</p>
                 <button
                     className=" h-8 w-8 p-1 flex items-center justify-center text-white text-sm rounded-md bg-indigo-300 hover:bg-indigo-400"
                     onClick={signOut}
